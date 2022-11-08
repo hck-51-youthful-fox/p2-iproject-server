acb6882b1b4b73a02cd3a77c5ccc04b2d6ebcc66
bbins <avinskat@gmail.com>
2022-11-09 04:28:03 +0700

    add user register

---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router()
+
+const { authenticate } = require('../middlewares/authenticate');
+// const { authorizeCustomer } = require('../middlewares/authorize');
+
+
+const userRouter = require('./users');
+const publicRouter = require('./public'); 
+const likeRouter = require('./like'); // favorites buat premium acc
+
+
+router.use('/', userRouter)
+router.use('/pub', publicRouter)
+router.use(authenticate)
+router.use('/likes', likeRouter)
+
+module.exports = router;
