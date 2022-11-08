ff3ad1b6a5f0fcc7c33836c57b758cd778e95c1c
Ridho Hauzan <dotcomhomo@gmail.com>
2022-11-08 16:01:29 +0700

    User BeforeCreate Hooks

--- Controller/controllers.js
+++ Controller/controllers.js
@@ -1,7 +1,14 @@
 const { User, Comment, Post } = require("../models/index");
 
 class Controller {
-
+    static async registerUser (req, res) {
+        let { email, username, password } = req.body
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
 }
 
 module.exports = Controller
--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const { hashPassword } = require('../helpers/bcrypt');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -27,5 +28,10 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
+  User.beforeCreate((instance) => {
+    instance.password = hashPassword(instance.password)
+    instance.isPremium = false
+  })
   return User;
 };
