4e1595784588d92af4d5398d6675d473ee7d942f
Rahmat <rahmathikaru03@gmail.com>
2022-11-08 14:00:44 +0700

    added feature register, login and google login

--- routers/index.js
+++ routers/index.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Controller = require("../controllers/controllers");
+const authUser = require("../middlewares/authentification");
+const errorsHandler = require("../middlewares/errorHandlers");
+
+router.post("/register", Controller.register);
+router.post("/login", Controller.login);
+router.post("/google-sign-in", Controller.googleSignIn);
+
+router.use(authUser);
+
+router.use(errorsHandler);
 
 module.exports = router;
