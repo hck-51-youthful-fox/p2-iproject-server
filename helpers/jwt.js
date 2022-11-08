adf750dfe1dae687f68ce3895d9b515266db4681
Ridho Hauzan <dotcomhomo@gmail.com>
2022-11-08 16:29:12 +0700

    revert change in main

--- helpers/jwt.js
+++
@@ -1,6 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const createToken = (payload) => jwt.sign(payload, "secret");
-const verifyToken = (token) => jwt.verify(token, "secret");
-
-module.exports = { createToken, verifyToken };
