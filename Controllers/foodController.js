c47839a951f08d352dfd86354184ed27b30b7ab0
Yossy Adirta <yossyadirta@gmail.com>
2022-11-08 16:48:11 +0700

    Add register and login for customer. Feature done

--- Controllers/foodController.js
+++ Controllers/foodController.js
@@ -97,6 +97,91 @@ class FoodController {
       next(error);
     }
   }
+
+  static async readAllFoods(req, res, next) {
+    try {
+      let options = {};
+      options.include = [
+        {
+          model: Category,
+        },
+      ];
+
+      let { page, filter, search } = req.query;
+      let limit = 8;
+      let offset = 0;
+      let totalPage;
+
+      options.order = [["id", "DESC"]];
+      options.include = [{ model: Category }];
+
+      // pagination
+      if (page !== "" && typeof page !== "undefined") {
+        limit = 8;
+        options.limit = limit;
+        if (page.number !== "" && typeof page.number !== "undefined") {
+          let offset = page.number * limit - limit;
+          options.offset = offset;
+        } else {
+          limit = 8;
+          offset = 0;
+          page.number = 1;
+          totalPage = 1;
+          options.limit = limit;
+          options.offset = offset;
+        }
+      }
+
+      if (!page) {
+        page = {};
+        totalPage = 1;
+        page.number = 1;
+      }
+
+      // filter by category
+      if (filter) {
+        if (filter !== "" && typeof filter !== "undefined") {
+          options.include = [
+            {
+              model: Category,
+              where: {
+                name: {
+                  [Op.eq]: filter.category,
+                },
+              },
+            },
+          ];
+        }
+      }
+
+      //search
+      if (search) {
+        options = {
+          limit: limit,
+          offset: offset,
+          order: [["id", "ASC"]],
+          where: {
+            [Op.and]: [{ name: { [Op.iLike]: `%${search}%` } }],
+          },
+        };
+      }
+
+      const data = await Food.findAndCountAll(options);
+
+      if (!data || data.rows.length === 0) {
+        throw { name: "FOOD_NOT_FOUND", model: "Foods" };
+      }
+      totalPage = data.count;
+      data.totalPage = Math.ceil(totalPage / limit);
+      if (!data.totalPage) {
+        data.totalPage = 1;
+      }
+      data.currentPage = page.number ? +page.number : 0;
+      res.status(200).json(data);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = FoodController;
