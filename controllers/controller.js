1f6592ab2069b61b66e86dc66bc6f03a8a949ffc
valtjoker <adjiugrohoseno@gmail.com>
2022-11-08 15:21:37 +0700

    added login-register feature

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,6 +3,45 @@ const { inputStatus } = require("../helpers/bcrypt");
 const { getToken, tokenVerif } = require("../helpers/jwt");
 const { Op } = require("sequelize");
 const { OAuth2Client } = require("google-auth-library");
-class Controller {}
+class Controller {
+  static async register(req, resp, next) {
+    try {
+      const { username, email, password, phoneNumber, address } = req.body;
+      const addUser = await User.create({
+        username,
+        email,
+        password,
+        phoneNumber,
+        address,
+      });
+      resp.status(201).json({
+        msg: "Account has been created",
+        id: addUser.id,
+        username: addUser.username,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async login(req, resp, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) throw { name: "user_input_missing" };
+      const foundUser = await User.findOne({ where: { email } });
+      if (!foundUser) throw { name: "invalid_credential" };
+      const status = inputStatus(password, foundUser.password);
+      if (!status) throw { name: "invalid_credential" };
+      const access_token = getToken({ id: foundUser.id });
+      resp.status(200).json({
+        access_token,
+        email: foundUser.email,
+        username: foundUser.username,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
 
 module.exports = Controller;
---
+++ routes/usersRoute.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const Controller = require("../controllers/controller");
+const router = express.Router();
+
+router.post("/register", Controller.register);
+router.post("/login", Controller.login);
