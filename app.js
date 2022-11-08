88da0ee760a861812a25724f40baa28cf58ea583
Yossy Adirta <yossyadirta@gmail.com>
2022-11-08 14:48:17 +0700

    Revisi routes admin

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const app = express();
 const port = 3000;
-const routes = require("./routes/admin");
+const routes = require("./routes/index");
 const cors = require("cors");
 
 app.use(cors());
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,10 @@ const express = require("express");
 const AdminController = require("../controllers/adminController");
 const router = express.Router();
 
-router.post("/admin/register", AdminController.adminRegister);
-router.post("/admin/login", AdminController.adminLogin);
+router.post("/register", AdminController.adminRegister);
+router.post("/login", AdminController.adminLogin);
+
+const categoryRoutes = require("./categories");
+router.use("/category", categoryRoutes);
 
 module.exports = router;
