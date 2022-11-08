e591a288bc5e7af3b3ceefa0cc7c41b31d2f674e
ivanarifin <ivan.rarifin@gmail.com>
2022-11-08 13:45:10 +0700

    login user done

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Controller = require("../controllers/user")
 
 router.post("/register", Controller.register)
+router.post("/login", Controller.login)
 
 
 module.exports = router;
