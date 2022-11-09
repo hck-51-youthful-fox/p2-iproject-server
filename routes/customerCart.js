d74de44884145a3a05e32325e96f96a3c19e5764
Yossy Adirta <yossyadirta@gmail.com>
2022-11-10 03:15:00 +0700

    Add new routes for update cart status

--- routes/customerCart.js
+++ routes/customerCart.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const CartController = require("../controllers/cartController");
 
 router.get("/", CartController.readCart);
+router.patch("/", CartController.updateCartStatusAfterPayment);
 router.get("/city", CartController.city);
 router.post("/cost", CartController.cost);
 router.post("/:foodId", CartController.addFoodToCart);
