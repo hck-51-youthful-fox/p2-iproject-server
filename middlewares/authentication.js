343fa346d290a3288016d9dc1e89c8e2d09f0a75
Ridho Hauzan <dotcomhomo@gmail.com>
2022-11-09 08:48:46 +0700

    Authentication Done

---
+++ middlewares/authentication.js
@@ -0,0 +1,38 @@
+const { verifyToken } = require("../helpers/jwt");
+const { User } = require("../models/index");
+
+const authentication = async (req, res, next) => {
+  try {
+    let { access_token } = req.headers;
+    // console.log(req.headers);
+    if (!access_token) {
+      throw {
+        message: "You are not authorized",
+      };
+    }
+
+    const validateToken = verifyToken(access_token);
+    if (!validateToken) {
+      throw {
+        message: "Invalid token",
+      };
+    }
+
+    const getUser = await User.findByPk(validateToken.id);
+    if (!getUser) {
+      throw {
+        message: "You are not authorized",
+      };
+    }
+
+    req.user = {
+      id: validateToken.id,
+    };
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(403).json(error);
+  }
+};
+
+module.exports = authentication;
