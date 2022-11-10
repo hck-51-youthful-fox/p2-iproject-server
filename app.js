dc7db6364f448d3d05fa6d444840fbe348a20833
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2022-11-10 12:24:20 +0700

    change production

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const cors = require("cors");
 const express = require("express");
 const router = require("./router/index");
 const ErrorHandler = require("./middleware/errorHandler");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
