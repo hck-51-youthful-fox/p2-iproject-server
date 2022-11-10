6d266089fb0827ec46a2195d4e8efc7f6a66c9ea
Rizqi irfan <r.irfannawwaf@gmail.com>
2022-11-10 11:29:57 +0700

    uncomment features

--- app.js
+++ app.js
@@ -27,36 +27,36 @@ const webSocket = new WebSocket(
   "wss://ws.finnhub.io?token=cdl1rqiad3i4r9fur7d0cdl1rqiad3i4r9fur7dg"
 );
 
-// function appendData(s, p, t) {
-//   const db = getDatabase();
-//   set(ref(db, "stocks/" + s + "/" + t), {
-//     p,
-//   });
-//   // console.log("updated");
-// }
+function appendData(s, p, t) {
+  const db = getDatabase();
+  set(ref(db, "stocks/" + s + "/" + t), {
+    p,
+  });
+  // console.log("updated");
+}
 
-// webSocket.on("open", function open() {
-//   webSocket.send(JSON.stringify({ type: "subscribe", symbol: "AAPL" }));
-//   webSocket.send(JSON.stringify({ type: "subscribe", symbol: "GOOG" }));
-//   webSocket.send(JSON.stringify({ type: "subscribe", symbol: "MSFT" }));
-//   webSocket.send(JSON.stringify({ type: "subscribe", symbol: "TSLA" }));
-//   webSocket.send(JSON.stringify({ type: "subscribe", symbol: "AMZN" }));
-//   webSocket.send(
-//     JSON.stringify({ type: "subscribe", symbol: "BINANCE:BTCUSDT" })
-//   );
+webSocket.on("open", function open() {
+  webSocket.send(JSON.stringify({ type: "subscribe", symbol: "AAPL" }));
+  webSocket.send(JSON.stringify({ type: "subscribe", symbol: "GOOG" }));
+  webSocket.send(JSON.stringify({ type: "subscribe", symbol: "MSFT" }));
+  webSocket.send(JSON.stringify({ type: "subscribe", symbol: "TSLA" }));
+  webSocket.send(JSON.stringify({ type: "subscribe", symbol: "AMZN" }));
+  webSocket.send(
+    JSON.stringify({ type: "subscribe", symbol: "BINANCE:BTCUSDT" })
+  );
 
-//   console.log("connected");
-// });
+  console.log("connected");
+});
 
-// webSocket.on("message", function message(data) {
-//   const input = JSON.parse(data);
-//   if (input.data) {
-//     console.log(input.data);
-//     const tes = input.data.map((el) => {
-//       appendData(el.s, el.p, el.t);
-//     });
-//   } else console.log(input);
-// });
+webSocket.on("message", function message(data) {
+  const input = JSON.parse(data);
+  if (input.data) {
+    console.log(input.data);
+    const tes = input.data.map((el) => {
+      appendData(el.s, el.p, el.t);
+    });
+  } else console.log(input);
+});
 
 //code express
 app.post("/register", async (req, res, next) => {
