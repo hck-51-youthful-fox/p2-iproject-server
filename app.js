49d26b58c03d8b244fd616fbdc084efd06b05c96
ivanarifin <ivan.rarifin@gmail.com>
2022-11-10 10:48:19 +0700

    update cors options

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const routes = require("./routes/index");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(
+  cors({
+    origin: ["https://iprojectp2-1c9e3.web.app"],
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+    origin: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
